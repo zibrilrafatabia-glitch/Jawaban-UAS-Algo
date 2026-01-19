ff7680ea456266bfd68918d898d75a9b430cf7f6
zibrilrafatabia-glitch <zibrilrafatabia@gmail.com>
2026-01-19 09:55:27 +0700

    jawaban

---
+++ SoalUAS4.java
@@ -0,0 +1,14 @@
+public class SoalUAS4 {
+    public static void main(String[] args) {
+        int n = 5;
+        int i, j;
+
+        // Lengkapi kode berikut :
+        for (i = 0; i < n; i++) {
+            for (j = 0; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
